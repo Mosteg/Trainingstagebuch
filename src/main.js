0e57530aeaeff475667ecef8210cc59c28b3abd2
Dein Name <deine.email@example.com>
2025-01-28 23:27:26 +0100

    renaming styles

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import ('./styles/style.css');
 import ('./styles/header.css');
-import ('./styles/style-login.css');
-import ('./styles/style-statistic.css');
-import ('./styles/style-userstats.css');
+import ('./styles/login.css');
+import ('./styles/statistics.css');
+import ('./styles/userstats.css');
 
 import Login from "./login.js";
 const login = new Login();
