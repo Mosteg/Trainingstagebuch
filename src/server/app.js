53b3bca5e3fb7f35ac7826827a10ad08444b73ad
Dein Name <deine.email@example.com>
2025-02-07 21:40:29 +0100

    grundlegende datenbank querys hinzugefügt

--- src/server/app.js
+++ src/server/app.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const Datenbank = require('./datenbank');
+db = new Datenbank();
+let user;
 
 const app = express();
 const server = app.listen(3000);
@@ -15,3 +18,36 @@ app.get('/', (req, res) => {
 app.use((req, res) => {
     res.status(404).render('404');
 });
+
+
+// db.getWorkoutsByUserId(1).then((result, fields) => {
+//     if(result[0] === undefined) throw new Error('Passwort oder Username falsch');
+//     else {
+//         console.log(result, fields);  
+//     }
+// }).catch(err => {
+//     console.log(err);
+// });
+
+// db.createAccount(db.createUser('12345', 'test2@gmail.com', 'Test2', 'test', 'test')).then((result) => {
+//     console.log(result);
+// }).catch(err => {
+//     console.log(err);
+// });
+
+// db.getUserByEmail('test2@gmail.com', '12345').then((result, fields) => {
+//     if(result[0] === undefined) throw new Error('Passwort oder Username falsch');
+//     else {
+//         user = result[0];
+//         console.log(user);  
+//     }
+// }).catch(err => {
+//     console.log(err);
+// });
+
+
+
+// für zugriff von extern, port muss freigegeben werden
+// app.listen(3000, '0.0.0.0', () => {
+
+// });
